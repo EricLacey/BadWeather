a695bfd805f11d5c99d2fb4b00c82c81d3445515
Eric Lacey <34018178+EricLacey@users.noreply.github.com>
2020-02-07 15:29:34 -0500

    implemented a countdown timer

--- public/assets/js/canvasCode.js
+++ public/assets/js/canvasCode.js
@@ -45,3 +45,31 @@ function clearArea() {
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
+
+function timer() {
+    // Set the date we're counting down to
+    let secs = 30
+    let now = new Date().getTime();
+    let endTime = new Date().setTime(now + (secs * 1000))
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get today's date and time
+        now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = endTime - now;
+
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("timer").innerHTML = seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "EXPIRED";
+        }
+    }, 500);
+}
