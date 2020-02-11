402c25198790c404cc7ad94354d0887e46d877f5
Eric Lacey <34018178+EricLacey@users.noreply.github.com>
2020-02-10 23:54:20 -0500

    noved timer to in-page script so that it could use socket

--- public/assets/js/canvasCode.js
+++ public/assets/js/canvasCode.js
@@ -46,37 +46,3 @@ function clearArea() {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
 
-function timer() {
-    // Set the date we're counting down to
-    let secs = 10;
-    let now = new Date().getTime();
-    let endTime = new Date().setTime(now + (secs * 1000))
-
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-
-        // Get today's date and time
-        now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        var distance = endTime - now;
-
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Display the result in the element with id="demo"
-        document.getElementById("timer").innerHTML = seconds + "s ";
-
-        // If the count down is finished, write some text
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("timer").innerHTML = "Done!";
-            
-            // save canvas image as data url (png format by default)
-            var canvas = document.getElementById('myCanvas')
-            var dataURL = canvas.toDataURL();
-
-            document.getElementById('canvasImg').src = dataURL;
-
-        }
-    }, 500);
-}
