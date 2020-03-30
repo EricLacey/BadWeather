9b4dd0ed45d7977ac144e5435b76e1533fdea919
antoinella <antoinella.dee@gmail.com>
2020-03-30 13:46:56 -0400

    Doesn't work... artifact img hint doesn't show up anymore after trying to make it random

---
+++ public/assets/js/randomArtifact.js
@@ -0,0 +1,15 @@
+//https://stackoverflow.com/questions/21648147/selecting-a-random-image
+
+let randomArtifact = new Array();
+randomArtifact[0] = 'paper01.png';
+randomArtifact[1] = 'paper02.png';
+randomArtifact[2] = 'paper03.png';
+randomArtifact[3] = 'paper04.png';
+randomArtifact[4] = 'paper05.png';
+randomArtifact[5] = 'paper06.png';
+
+function randomizeArtifacts() {
+    var num = Math.floor( Math.random() * 6);
+    var img = randomArtifact[num];
+    document.getElementById("artifactHint").innerHTML = ('<img src="' + 'assets/userInterface/' + img)
+}
