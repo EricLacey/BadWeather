06e251320f7280538760bca76ab0c657a012a640
antoinella <antoinella.dee@gmail.com>
2020-03-31 15:11:41 -0400

    updated array so that it holds objects

--- public/assets/js/randomArtifact.js
+++ public/assets/js/randomArtifact.js
@@ -1,15 +1,33 @@
 //https://stackoverflow.com/questions/21648147/selecting-a-random-image
+//https://www.w3schools.com/js/js_arrays.asp
 
+//objects
+const anubis = {
+    'imageName': '1.png'
+}
+const horus = {
+    'imageName': '2.png'
+}
+const isis = {
+    'imageName': '3.png'
+}
+const whalehat = {
+    'imageName': '4.png'
+}
+const moai = {
+    'imageName': '5.png'
+}
+
+//array of objects
 let randomArtifact = new Array();
-randomArtifact[0] = 'paper01.png';
-randomArtifact[1] = 'paper02.png';
-randomArtifact[2] = 'paper03.png';
-randomArtifact[3] = 'paper04.png';
-randomArtifact[4] = 'paper05.png';
-randomArtifact[5] = 'paper06.png';
+randomArtifact[0] = anubis;
+randomArtifact[1] = horus;
+randomArtifact[2] = isis;
+randomArtifact[3] = whalehat;
+randomArtifact[4] = moai;
 
 function randomizeArtifacts() {
-    var num = Math.floor( Math.random() * 6);
-    var img = randomArtifact[num];
-    document.getElementById("artifactHint").innerHTML = ('<img src="' + 'assets/userInterface/' + img + '" width="600px">')
+    let num = Math.floor( Math.random() * 5);
+    let chosenArtifact = randomArtifact[num];
+    document.getElementById("artifactHint").innerHTML = ('<img src="' + 'assets/userInterface/' + chosenArtifact.imageName + '" width="600px">')
 }
