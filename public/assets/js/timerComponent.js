c9f882078cc4f8fe498a6b658f9acd8584391770
antoinella <antoinella.dee@gmail.com>
2020-03-31 12:56:16 -0400

    1 min timer displays now
    had to parent it to camera

--- public/assets/js/timerComponent.js
+++ public/assets/js/timerComponent.js
@@ -2,20 +2,19 @@
 
 AFRAME.registerComponent('timer', {
     schema: {
-      TimeOutTime : {type:'int', default:300 }, //LENGTH OF TIMER:  5 minutes
+      TimeOutTime : {type:'int', default:60 }, //LENGTH OF TIMER
       DigitsColor: {type: 'color', default: '#000"'},
     },
     init: function () {
-      var data = this.data; //get all the data from the schema. 
-      var el = this.el; //get reference to the entity.
-      this.paused= false;  //to handle playing/pausing the timer 
+      var data = this.data;
+      var el = this.el;
   
-      var date= new Date(); // to get current time
-      this.TargetTime=new Date(date.getTime() + data.TimeOutTime*1000); //calulate the target time
+      var date= new Date(); 
+      this.TargetTime=new Date(date.getTime() + data.TimeOutTime*1000);
        
-       seconds = new THREE.Object3D(); //the parent object
+       seconds = new THREE.Object3D();
        seconds.name="seconds"
-       for(var j=0;j<2;j++)  //to Initialize and Place all the 7 parts of a Digit , twice 
+       for(var j=0;j<2;j++)
       {
        distance = -j*0.25;
        parent1 = new THREE.Object3D();
@@ -85,7 +84,7 @@ AFRAME.registerComponent('timer', {
          parent7.position.y-=0.07;
          seconds.add(parent7);
         }
-        el.setObject3D('TimerMesh', seconds); //setting the initialized object(seconds) to the our entity 
+        el.setObject3D('TimerMesh', seconds); 
     },
     tick : function() 
     { 
@@ -103,10 +102,8 @@ AFRAME.registerComponent('timer', {
     play: function () {
       console.log("play called");
     },
-    Setdigit: function(){  //set the digits to the updated time left. 
-     
-      var digitval= [[1,1,1,0,1,1,1],[0,0,1,0,0,1,0],[0,1,1,1,1,0,1],[0,1,1,1,1,1,0],[1,0,1,1,0,1,0],[1,1,0,1,1,1,0],[1,1,0,1,1,1,1],[0,1,1,0,0,1,0],[1,1,1,1,1,1,1],[1,1,1,1,0,1,0]];
-     // console.log(seconds.children.length); 
+    Setdigit: function(){
+      var digitval= [[1,1,1,0,1,1,1],[0,0,1,0,0,1,0],[0,1,1,1,1,0,1],[0,1,1,1,1,1,0],[1,0,1,1,0,1,0],[1,1,0,1,1,1,0],[1,1,0,1,1,1,1],[0,1,1,0,0,1,0],[1,1,1,1,1,1,1],[1,1,1,1,0,1,0]]; 
       var tensPlace = Math.floor(this.seconda/10); 
       for(var a=0;a<7;a++) 
            { 
@@ -153,16 +150,13 @@ AFRAME.registerComponent('timer', {
       var data = this.data;
       var el = this.el;
    
-      // If `oldData` is empty, then this means we're in the initialization process.
-      // No need to update.
     if (Object.keys(oldData).length === 0) { return; }
       console.log("update "+ data.DigitsColor);
-      // Material-related properties changed. Update the material.
       if (data.DigitsColor !== oldData.DigitsColor) {
         console.log(" update "+ seconds.children[1].isObject3D+" "+ data.DigitsColor.toString(16).slice(-6) );
-        //we are slicing color as the returned color is of the form "#000000" and we require only the digits.
+        
         var col= "0x"+data.DigitsColor.toString(16).slice(-6);
-        for(var i=0;i<14;i++) //updating the color of all the children of entity
+        for(var i=0;i<14;i++)
         {
           el.object3D.children[0].children[i].children[0].material.color.setHex(col);
         }
