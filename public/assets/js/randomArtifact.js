0b3994d096dd7b8f274dd58df98c08dd8f393ed3
Arina <43216874+rereshek@users.noreply.github.com>
2020-04-01 01:11:23 -0400

    game over sections added

--- public/assets/js/randomArtifact.js
+++ public/assets/js/randomArtifact.js
@@ -4,23 +4,28 @@
 //objects
 const anubis = {
     'imageName': '1.png',
-    'artifactName': 'anubis'
+    'artifactName': 'anubis',
+    'fact': 'This Canopic Jar represents Anubis - the Egyptian god of mummification and the afterlife. It is made using limestone/pottery and placed in a chest, being buried in the corpse’s tomb. Inside, this jar contains stomach, intestinesm lungs and liver. The Heart was left with the corpse because the Egyptians believed it was the soul. Many jars survived from the old Kingdom and are displayed in today’s museums.'
 }
 const horus = {
     'imageName': '2.png',
-    'artifactName': 'horus'
+    'artifactName': 'horus',
+    'fact': 'This Canopic Jar represents Horus - the important and ancient deity of Egyptian gods. It is made using limestone/pottery and placed in a chest, being buried in the corpse’s tomb. Inside this jar contains stomach, intestinesm lungs and liver. The Heart was left with the corpse because the Egyptians believed it was the soul. Many jars survived from the old Kingdom and are displayed in today’s museums.'
 }
 const isis = {
     'imageName': '3.png',
-    'artifactName': 'isis'
+    'artifactName': 'isis',
+    'fact': 'This Canopic Jar represents Isis - a goddess of life and magic in Egyptian mythology. It is made using limestone/pottery and placed in a chest, being buried in the corpse’s tomb. Inside this jar contains stomach, intestinesm lungs and liver. The Heart was left with the corpse because the Egyptians believed it was the soul. Many jars survived from the old Kingdom and are displayed in today’s museums.'
 }
 const whalehat = {
     'imageName': '4.png',
-    'artifactName': 'whalehat'
+    'artifactName': 'whalehat',
+    'fact': 'This is a Whale Hat that represents a war helmet. Such hats came from the Tlingit Indians and were worn by clans. The Killer whale hat embodies clan ancestors and allows tribe members to feel their presence'
 }
 const moai = {
     'imageName': '5.png',
-    'artifactName': 'moai'
+    'artifactName': 'moai',
+    'fact': 'This is a Moai statue that came from Polynesia. It is made from volcanic rock and is 13 ft in average height! Such statues represent large oversized human head and symbolize religion, political power, leadership. '
 }
 
 //array of objects
@@ -31,8 +36,14 @@ randomArtifact[2] = isis;
 randomArtifact[3] = whalehat;
 randomArtifact[4] = moai;
 
+let num = Math.floor( Math.random() * 5);
+let chosenArtifact = randomArtifact[num];
+
 function randomizeArtifacts() {
-    let num = Math.floor( Math.random() * 5);
-    let chosenArtifact = randomArtifact[num];
+
     document.getElementById("artifactHint").innerHTML = ('<img src="' + 'assets/userInterface/' + chosenArtifact.imageName + '" width="600px">')
 }
+
+
+      
+    
