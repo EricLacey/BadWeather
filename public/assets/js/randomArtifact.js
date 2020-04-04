ab3b67e0481b11ad17ad8c2a1e17b81233613136
ConorMulcahy118 <cemulcahy@gmail.com>
2020-04-04 14:00:52 -0400

    final few fixes

--- public/assets/js/randomArtifact.js
+++ public/assets/js/randomArtifact.js
@@ -5,31 +5,31 @@
 const anubis = {
     'imageName': '1.png',
     'artifactName': 'anubis',
-    'fact': 'This Canopic Jar represents Anubis - the Egyptian god of mummification and the afterlife. It is made using limestone/pottery and placed in a chest, being buried in the corpse’s tomb. Inside, this jar contains stomach, intestinesm lungs and liver. The Heart was left with the corpse because the Egyptians believed it was the soul. Many jars survived from the old Kingdom and are displayed in today’s museums.',
+    'fact': 'Represented by the jackal-headed god Duamutef, this canopic jar was an ancient Egyptian funerary urn used to contain the stomach of a mummified body to protect it during its journey into the afterlife. Typically made from limestone or calcite, canopic jars like this are a common staple in Egyptology and can be seen in museums the world over.',
     'group' : 'egypt'
 }
 const horus = {
     'imageName': '2.png',
     'artifactName': 'horus',
-    'fact': 'This Canopic Jar represents Horus - the important and ancient deity of Egyptian gods. It is made using limestone/pottery and placed in a chest, being buried in the corpse’s tomb. Inside this jar contains stomach, intestinesm lungs and liver. The Heart was left with the corpse because the Egyptians believed it was the soul. Many jars survived from the old Kingdom and are displayed in today’s museums.',
+    'fact': 'Represented by the falcon-headed god Qebehsenuef, this canopic jar was an ancient Egyptian funerary urn used to contain the intestines of a mummified body to protect it during its journey into the afterlife. Typically made from limestone or calcite, canopic jars like this are a common staple in Egyptology and can be seen in museums the world over.',
     'group' : 'egypt'
 }
 const isis = {
     'imageName': '3.png',
     'artifactName': 'isis',
-    'fact': 'This Canopic Jar represents Isis - a goddess of life and magic in Egyptian mythology. It is made using limestone/pottery and placed in a chest, being buried in the corpse’s tomb. Inside this jar contains stomach, intestinesm lungs and liver. The Heart was left with the corpse because the Egyptians believed it was the soul. Many jars survived from the old Kingdom and are displayed in today’s museums.',
+    'fact': 'Represented by the human-headed god Imsety, this canopic jar was an ancient Egyptian funerary urn used to contain the liver of a mummified body to protect it during its journey into the afterlife. Typically made from limestone or calcite, canopic jars like this are a common staple in Egyptology and can be seen in museums the world over.',
     'group' : 'egypt'
 }
 const whalehat = {
     'imageName': '4.png',
     'artifactName': 'whalehat',
-    'fact': 'This is a Whale Hat that represents a war helmet. Such hats came from the Tlingit Indians and were worn by clans. The Killer whale hat embodies clan ancestors and allows tribe members to feel their presence.',
+    'fact': 'This wooden hat belongs to the Tlingit Daklaweidi (Killer Whale) clan of southeast Alaska. It is a sacred object representing their clan crest of a whale rising from the ocean. Repatriated to the clan in 2005, the hat is also an important symbol of cultural patrimony.',
     'group' : 'aboriginal'
 }
 const moai = {
     'imageName': '5.png',
     'artifactName': 'moai',
-    'fact': 'This is a Moai statue that came from Polynesia. It is made from volcanic rock and is 13 ft in average height! Such statues represent large oversized human head and symbolize religion, political power, leadership.',
+    'fact': 'Monolithic stone sculptures standing up to 10 meters in height, Moai statues were carved by the Rapa Nui people of Easter Island in Polynesia to deify their ancestors. The mystery of how the statues, weighing approximately 13 tons each, were moved around the island has been a topic of debate for centuries, and remains truly unsolved to this day.',
     'group' : 'moai'
 }
 
